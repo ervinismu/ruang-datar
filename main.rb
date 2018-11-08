46b662c113e37de3aa2b5a9862257d250dde4136
alvianzf <alvianzf@gmail.com>
2018-11-08 12:11:18 +0700

    [alvianzf] update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require './layang-layang'
 require './persegi'
+require './belahketupat'
+require './trapesium'
 
 puts "SELAMAT DATANG"
 puts "=================================================================="
@@ -23,6 +25,57 @@ elsif
   print "masukkan sisi: "
   s = gets.to_i
   persegi = Persegi.new(s)
+elsif a==7
+    print "masukkan diagonal 1: "
+    d1 = gets.to_i
+    print "masukkan diagonal 2: "
+    d2 = gets.to_i
+    print "masukkan panjang sisi: "
+    s = gets.to_i
+    ket = BelahKetupat.new(d1,d2,s)
+elsif a==6
+    
+loop do
+    puts "Hitung Bangun Datar Trapesium"
+    puts "1). Luas"
+    puts "2). Keliling"
+    puts "3). Keluar"
+    print "pilih : "
+    choose = gets.to_i
+    
+    trapesium = Trapesium.new()
+    
+    if choose == 1
+        puts ""
+        puts "+++++++++++++++"
+        puts "+ Hitung Luas +"
+        puts "+++++++++++++++"
+        print "Masukkan nilai alas : "
+        a = gets.to_i
+        print "Masukkan nilai yang sejajar dengan alas : "
+        b = gets.to_i
+        print "Masukkan nilai tinggi : "
+        t = gets.to_i
+    
+        trapesium.luas(a,b,t)
+    elsif choose == 2
+        puts ""
+        puts "+++++++++++++++"
+        puts "+   Keliling  +"
+        puts "+++++++++++++++"
+        print "Masukkan nilai alas : "
+        a = gets.to_i
+        print "Masukkan nilai yang sejajar dengan alas : "
+        b = gets.to_i
+        print "Masukkan nilai sisi miring : "
+        c = gets.to_i
+        print "Masukkan nilai sisi miring : "
+        d = gets.to_i
+    
+        trapesium.keliling(a,b,c,d)
+    end
+    break if choose == 3
+    end
 else
     puts "masukan salah"
 end
